a7633cf8f1f71df607e40ce935f9a6c1c6e751d3
Alan Rosas Palacios <43190667+trimaxlan98@users.noreply.github.com>
2025-03-31 19:38:54 -0600

    cam

--- build_app.sh
+++ build_app.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Activar entorno virtual si estás usando uno
+# source venv/bin/activate
+
+# Ejecutar PyInstaller
 python3 -m PyInstaller --name="GestionClientes" --onedir --windowed \
     --add-data "clientes/templates:clientes/templates" \
     --add-data "clientes/static:clientes/static" \
@@ -7,3 +13,5 @@ python3 -m PyInstaller --name="GestionClientes" --onedir --windowed \
     --hidden-import=django.contrib.sessions \
     --hidden-import=django.contrib.messages \
     run_app.py
+
+echo "Proceso de empaquetado completado."
